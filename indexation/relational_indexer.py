e8b1245a8ca2d3959ac3725aea71e078a127fbb2
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2018-06-01 12:59:34 +0200

    fixed thresholding

--- indexation/relational_indexer.py
+++ indexation/relational_indexer.py
@@ -160,16 +160,18 @@ class RelationalTextIndex(BaseTextIndex):
         session = Session()     # no need to create session scope
         query = session.query(Query) \
                        .filter(Query.query_id==query_name) \
-                       .filter(Query.matches.any(Match.score) >= threshold)
+                       .join(Query.matches, aliased=True) \
+                       .filter(Match.score >= float(threshold))
 
-        for q in query.yield_per(100):
+        for q in query.yield_per(100):  # paginate over 100 items
             match = {'target': q.text,
                      'meta': {'path': q.path, 'num': q.num},
                      'matches': []}
 
             for m in session.query(Match).filter_by(query_id=q.id).all():
-                source, smeta = self.get_indexed_text(m.source)
-                match['matches'].append(
-                    {'source': source, 'score': m.score, 'meta': smeta})
+                if m.score >= threshold:
+                    source, smeta = self.get_indexed_text(m.source)
+                    match['matches'].append(
+                        {'source': source, 'score': m.score, 'meta': smeta})
 
             yield match
